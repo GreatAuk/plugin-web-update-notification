4f705b8f3d277a40a82e8fa3b0ddb4fea12aa48f
Utopia <greatauk11@gmail.com>
2022-06-21 16:09:12 +0800

    chore: add react example

---
+++ example/react/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { webUpdateNotice } from 'vite-plugin-web-update-notification'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    webUpdateNotice({
+      logHash: true,
+      checkInterval: 0.5 * 60 * 1000,
+    }),
+  ],
+})
